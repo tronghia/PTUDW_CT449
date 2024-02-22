0ce082b7639352a1a22906a07170ca39560fcf19
tronghia <ttnghia5112002@gmail.com>
2024-02-22 15:45:26 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -8,6 +9,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not fond"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
